8082846547ab74ccf8ff4241cea6c2aee0f538b0
chenmi1997 <46470453+chenmi1997@users.noreply.github.com>
2019-02-03 15:15:35 -0500

    base

---
+++ shopping_cart.py
@@ -0,0 +1,78 @@
+# shopping_cart.py
+
+import datetime
+
+t = datetime.datetime.now()
+
+print("STARTED AT: " + str(t))
+
+print(t.strftime("%Y-%m-%d"))
+
+products = [
+    {
+        "id":1, 
+        "name": "Chocolate Sandwich Cookies", 
+        "department": "snacks", 
+        "aisle": "cookies cakes", 
+        "price": 3.50
+    },
+    
+    
+    {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
+    {"id":3, "name": "Robust Golden Unsweetened Oolong Tea", "department": "beverages", "aisle": "tea", "price": 2.49},
+    {"id":4, "name": "Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce", "department": "frozen", "aisle": "frozen meals", "price": 6.99},
+    {"id":5, "name": "Green Chile Anytime Sauce", "department": "pantry", "aisle": "marinades meat preparation", "price": 7.99},
+    {"id":6, "name": "Dry Nose Oil", "department": "personal care", "aisle": "cold flu allergy", "price": 21.99},
+    {"id":7, "name": "Pure Coconut Water With Orange", "department": "beverages", "aisle": "juice nectars", "price": 3.50},
+    {"id":8, "name": "Cut Russet Potatoes Steam N' Mash", "department": "frozen", "aisle": "frozen produce", "price": 4.25},
+    {"id":9, "name": "Light Strawberry Blueberry Yogurt", "department": "dairy eggs", "aisle": "yogurt", "price": 6.50},
+    {"id":10, "name": "Sparkling Orange Juice & Prickly Pear Beverage", "department": "beverages", "aisle": "water seltzer sparkling water", "price": 2.99},
+    {"id":11, "name": "Peach Mango Juice", "department": "beverages", "aisle": "refrigerated", "price": 1.99},
+    {"id":12, "name": "Chocolate Fudge Layer Cake", "department": "frozen", "aisle": "frozen dessert", "price": 18.50},
+    {"id":13, "name": "Saline Nasal Mist", "department": "personal care", "aisle": "cold flu allergy", "price": 16.00},
+    {"id":14, "name": "Fresh Scent Dishwasher Cleaner", "department": "household", "aisle": "dish detergents", "price": 4.99},
+    {"id":15, "name": "Overnight Diapers Size 6", "department": "babies", "aisle": "diapers wipes", "price": 25.50},
+    {"id":16, "name": "Mint Chocolate Flavored Syrup", "department": "snacks", "aisle": "ice cream toppings", "price": 4.50},
+    {"id":17, "name": "Rendered Duck Fat", "department": "meat seafood", "aisle": "poultry counter", "price": 9.99},
+    {"id":18, "name": "Pizza for One Suprema Frozen Pizza", "department": "frozen", "aisle": "frozen pizza", "price": 12.50},
+    {"id":19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend", "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99},
+    {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
+] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
+
+# for p in products:
+    #  print(p)
+
+x = 1 # our counter variable
+
+while x < 5:
+    y = input("Please input a product id, or DONE if there are no more products: ")
+    print(y)
+    x = x + 1
+
+
+
+
+
+
+x = 1
+
+running_total = 0
+
+while x < 5:
+    selected_id = 1 # input("Please select a product id (1-20)")
+    #product = {
+    #    "id":1, 
+    #    "name": "Chocolate Sandwich Cookies", 
+    #    "department": "snacks", 
+    #    "aisle": "cookies cakes", 
+    #    "price": 3.50
+    #}
+    matchings_products = [p for p in products if p["id"] == selected_id]
+    product = matchings_products[0]
+    price = product["price"] # 4.95
+    running_total = running_total + price
+    x = x + 1
+
+print("THE TOTAL PRICE IS: " + str(running_total))
+
+# todo: calculate tax, add tax + total
